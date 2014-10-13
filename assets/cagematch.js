105fd57d6fb7c466f3b39b525ce7ffae38f78a55
Liam Schauerman <ljschauerman@gmail.com>
2014-10-13 12:26:31 -0700

    movieContainer populated with random movie

--- assets/cagematch.js
+++ assets/cagematch.js
@@ -30,4 +30,38 @@ var data = [
 			imdbId: 'tt0368891'
 		},
 	];
-	console.log(data)
+$(document).on('ready', function(){
+	$('.movieContainer').on('click', function(){
+		var nums = twoRandomNumbers();
+		renderMovie('#left', nums[0])
+		renderMovie('#right', nums[1])
+	});
+
+var twoRandomNumbers = function(){
+	var indexA = Math.floor(Math.random()*data.length);
+	var indexB = Math.floor(Math.random()*data.length);
+	while(indexA === indexB){ //no duplicates
+		indexB = Math.floor(Math.random()*data.length);
+	}
+	return [indexA, indexB];
+}
+
+//renderMovie takes a dom destination and the index corresponding with the movie
+var renderMovie = function(dest, index) {
+	// access the values inside of data[index]
+	// generage html to fill the div
+	var thisMovie = data[index];
+	var htmlTemplate = '<div class="title">'+thisMovie.title+'</div><div class="image"><img src="'+thisMovie.imgUrl+'"></div>'
+	$(dest).html(htmlTemplate)
+}
+
+
+
+
+
+
+
+}) //end ready block
+
+// todo:
+// adjust score
