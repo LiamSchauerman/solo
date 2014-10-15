401a251e9a20f9bbfb039ea5eafbc0db9ed868a1
Liam Schauerman <ljschauerman@gmail.com>
2014-10-14 18:00:18 -0700

    ready to present

--- client/cagematchNg.js
+++ client/cagematchNg.js
@@ -33,10 +33,10 @@ app.controller('MatchupController', function($scope, MakeMatchup, $firebase){
 	$scope.declareWinner = function(winner, loser){
 		var winExp = 1/(1+Math.pow(10, ( $scope.data[loser].score - $scope.data[winner].score )/400));
 		var K = 24;
-		console.log(winExp);
 		var winScore = $scope.data[winner].score + K*(1-winExp);
 		var diff = winScore - $scope.data[winner].score;
 		console.log("diff", diff)
+		$scope.diff = diff;
 		$scope.data[winner].score += Math.floor(diff);
 		$scope.data[loser].score -= Math.floor(diff);
 
@@ -45,12 +45,18 @@ app.controller('MatchupController', function($scope, MakeMatchup, $firebase){
 		$scope.data.$save($scope.data[loser])
 
 		$scope.inPlay = MakeMatchup.twoRandomNumbers();
+		$scope.toggleRankings = function() {
+			console.log($scope.rankings)
+			// show or hide the rankings div
+			$scope.rankings === true ? $scope.rankings = false : $scope.rankings = true;
+		}
 	};
 
 })
 
 app.factory('MakeMatchup', function(){
 
+	// generate two random index values
 	var twoRandomNumbers = function(){
 		var indexA = Math.floor(Math.random()*movies.length);
 		var indexB = Math.floor(Math.random()*movies.length);
@@ -58,22 +64,6 @@ app.factory('MakeMatchup', function(){
 			indexB = Math.floor(Math.random()*movies.length);
 		}
 		return [indexA, indexB];
-	}
-
-	var declareWinner = function(winner, loser) {
-		// movies[winner].score += 100;
-		// movies[loser].score -= 100;
-		var winExp = 1/(1+Math.pow(10, ( movies[loser].score - movies[winner].score )/400));
-		var K = 24;
-		console.log(winExp);
-		var winScore = movies[winner].score + K*(1-winExp);
-		var diff = winScore - movies[winner].score;
-		console.log("diff", diff)
-		movies[winner].score += Math.floor(diff);
-		movies[loser].score -= Math.floor(diff);
-
-
-		// find the amount added to winner, subtract that from loser
 	}
 
 	var movies = [
@@ -172,295 +162,13 @@ app.factory('MakeMatchup', function(){
 			imgUrl: 'images/theWickerMan.jpg',
 			imdbId: 'tt0450345',
 			score: 1000
-		},
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
-		// {
-		// 	title: '',
-		// 	imgUrl: 'images/',
-		// 	imdbId: '',
-		// 	score: 1000
-		// },
+		}
 	];
 	return {
 		twoRandomNumbers: twoRandomNumbers,
-		movies: movies,
-		declareWinner: declareWinner
+		movies: movies
+		// declareWinner: declareWinner
 	}
 
 })
-// var movies = [
-  // 	{
-  // 		title: 'Con Air',
-  // 		imgUrl: 'images/conAir.jpg',
-  // 		score: 1000,
-  // 		imdbId: 'tt0118880'
-  // 	},
-  // 	{
-  // 		title: 'The Rock',
-  // 		imgUrl: 'images/theRock.jpg',
-  // 		score: 1000,
-  // 		imdbId: 'tt0117500'
-  // 	},
-  // 	{
-  // 		title: 'Raising Arizona',
-  // 		imgUrl: 'images/raisingArizona.jpg',
-  // 		score: 1000,
-  // 		imdbId: 'tt0093822'
-  // 	},
-  // 	{
-  // 		title: 'Gone in 60 Seconds',
-  // 		imgUrl: 'images/goneIn60Seconds.jpg',
-  // 		score: 1000,
-  // 		imdbId: 'tt0187078'
-  // 	},
-  // 	{
-  // 		title: 'National Treasure',
-  // 		imgUrl: 'images/nationalTreasure.jpg',
-  // 		score: 1000,
-  // 		imdbId: 'tt0368891'
-  // 	},
-  // 	{
-  // 		title: 'The Boy in Blue',
-  // 		imgUrl: 'images/theBoyInBlue.jpg',
-  // 		imdbId: 'tt0090769',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Peggy Sue Got Married',
-  // 		imgUrl: 'images/peggySueGotMarried.jpg',
-  // 		imdbId: 'tt0091738',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Moonstruck',
-  // 		imgUrl: 'images/moonstruck.jpg',
-  // 		imdbId: 'tt0093565',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: "Vampire\'s Kiss",
-  // 		imgUrl: 'images/vampiresKiss.jpg',
-  // 		imdbId: 'tt0098577',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Time to Kill',
-  // 		imgUrl: 'images/timeToKill.jpg',
-  // 		imdbId: 'tt0100762',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Wild at Heart',
-  // 		imgUrl: 'images/wildAtHeart.jpg',
-  // 		imdbId: 'tt0100935',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Fire Birds',
-  // 		imgUrl: 'images/fireBirds.jpg',
-  // 		imdbId: 'tt0099575',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Honeymoon in Vegas',
-  // 		imgUrl: 'images/honeyMoonInVegas.jpg',
-  // 		imdbId: 'tt0104438',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Face-Off',
-  // 		imgUrl: 'images/faceOff.jpg',
-  // 		imdbId: 'tt0119094',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'Lord of War',
-  // 		imgUrl: 'images/lordOfWar.jpg',
-  // 		imdbId: 'tt0399295',
-  // 		score: 1000
-  // 	},
-  // 	{
-  // 		title: 'The Wicker Man',
-  // 		imgUrl: 'images/theWickerMan.jpg',
-  // 		imdbId: 'tt0450345',
-  // 		score: 1000
-  // 	}
-  // 	];
-  // for (var i=0; i < movies.length; i++) {
-  // 	console.log(movies.length)
-  // 	$scope.data.$add(movies[i])
-  // 	// debugger;
-  // };
+
