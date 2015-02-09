c09ceb1a052255967070195ff5eddc46f87ac727
Liam Schauerman <ljschauerman@gmail.com>
2015-02-08 23:45:03 -0800

    mongo refactor init

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
-// var http = require('http');
+var http = require('http');
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/cagematch');
 // var url = require('url');
 // var fs = require('fs');
 var bodyParser  = require('body-parser'),
@@ -10,13 +13,18 @@ app.set('view engine', 'html');
 // app.set('views', __dirname + '/client/views');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/client'));
+app.use(express.static(__dirname+"/client"));
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 // app.use(express.bodyParser());
 // app.use(app.router)
 
 
-var port = process.env.PORT || 4568;
+var port = process.env.PORT || 1337;
 app.listen(port);
+console.log("Server listening on ", port)
 
 
 app.get('/', function(req, res){
