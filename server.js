c4ab920d38737d0be8a00a643c4c4b54fe6f0f81
Liam Schauerman <ljschauerman@gmail.com>
2015-02-13 11:42:18 -0800

    changed to postgres

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var http = require('http');
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/cagematch');
-// var url = require('url');
-// var fs = require('fs');
-var bodyParser  = require('body-parser'),
+var url = require('url');
+var fs = require('fs');
+var bodyParser  = require('body-parser');
+var pg = require('pg')
+  , connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/postgres'
+  , client
+  , query;
+// var conString = "postgres://postgres:1234@localhost/postgres";
+var port = process.env.PORT || 1337;
+client = new pg.Client(connectionString);
+client.connect();
+// query = client.query('SELECT * FROM mytable');
+// query.on('end', function() { client.end(); });
+
 app = express();
+
 
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
@@ -14,50 +23,33 @@ app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname+"/client"));
-app.use(function(req,res,next){
-    req.db = db;
-    next();
-});
+
 // app.use(express.bodyParser());
 // app.use(app.router)
 
 
-var port = process.env.PORT || 1337;
-app.listen(port);
-console.log("Server listening on ", port)
 
 
-app.get('/', function(req, res){
-	res.render('index.html')
-})
+// app.get('/', function(req, res){
+// 	res.render('index.html')
+// })
+app.get('/', function(req, res) {
+  var date = new Date();
 
+  client.query('INSERT INTO mytable(date) VALUES($1)', [date]);
 
-// get mongo data
-app.get('/title', function(req, res){
-	var db = req.db;
-	var collection = db.get("generalInfo");
-	collection.find({},{}, function(e,docs){
-		if(e) console.log(e);
-		res.status(200).json({
-			"docresults" : docs
-		});
-	});
+  query = client.query('SELECT COUNT(date) AS count FROM mytable WHERE date = $1', [date]);
+  query.on('row', function(result) {
+    console.log(result);
+
+    if (!result) {
+      return res.send('No data found');
+    } else {
+      res.send('Visits today: ' + result.count);
+    }
+  });
 });
-app.post('/post', function(req,res){
-	var db = req.db;
-	var collection = db.get('generalInfo');
-	collection.insert({
-		"testKey" : "TestValue",
-		"TestObject" : {
-			"nested" : true
-		}
-	}, function(err, doc){
-		if( err ){
-			res.send("Error in post")
-		} else {
-			res.location('title');
-			res.redirect('title');
-		}
-	})
-})
-// app.post()
+
+app.listen(port, function(){
+	console.log("Server listening on ", port)
+});
